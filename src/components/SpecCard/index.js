4e8aa0c2e8f6f530c1ff98d1ba202110ada35920
bashar8606 <ahammed.bashar9@gmail.com>
2024-09-02 19:46:38 +0400

    latest

--- src/components/SpecCard/index.js
+++ src/components/SpecCard/index.js
@@ -10,19 +10,19 @@ const SpecCard = ({ data }) => {
         </div>
       </div>
       <div className={style.card_body}>
-        <h3 className={`fw-400 ${style.title}`}>{data?.title}</h3>
-        <p className="title-md">{data?.description}</p>
+        <h3 className={`fw-400 text-white ${style.title}`}>{data?.title}</h3>
+        <p className="title-md text-white opacity-50">{data?.description}</p>
       </div>
       <div className={`${style.card_footer}`}>
         {data?.tags?.map((item, i)=>{
             return(
                 <div key={i}
-                className={`d-inline-flex align-items-center ps-2 pe-3 py-1 rounded-pill border me-2 mb-2  `}
+                className={`d-inline-flex align-items-center ps-2 pe-3 py-1 rounded-pill border me-2 mb-2   ${style.tag}`}
               >
                 <div className={`ratio ratio-1x1 ${style.icon}`}>
-                  <Image src={data?.img} fill alt="" className="object-fit-contain" />
+                  <Image src={item?.img} fill alt="" className="object-fit-contain" />
                 </div>
-                <p className="mb-0 title-sm ps-2">Figma</p>
+                <p className="mb-0 title-sm ps-2 text-white">{item?.title}</p>
               </div>  
             )
         })}
--- src/widgets/HomeSkill/index.js
+++ src/widgets/HomeSkill/index.js
@@ -13,14 +13,14 @@ const HomeSkill = ({ id }) => {
     useHomeSkill({ style });
 
   return (
-    <section className={`sec-padding ${style.section}`} ref={main} id={id}>
+    <section className={`sec-padding ${style.section} pt-0`} ref={main} id={id}>
       <div className="container">
         <div className={`text-center  ${style.title_wrap}`}>
-          <p className="mb-0 h5">My Skills</p>
+          <p className="mb-0 h5 text-white">My Skills</p>
           <h2 className={`h1 ${style.title} fw-400`}>What I can do for you </h2>
         </div>
 
-        <div className="row row-cols-lg-3 gy-4">
+        <div className={`row row-cols-lg-3 ${style.data_wrap}`}>
           {datas?.map((item, i)=>{
             return(
               <div key={i}>
@@ -56,45 +56,40 @@ const datas = [
     description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  ut labore et dolore magna aliqua.",
     img:"/assets/images/icon-ui-fill.svg",
     tags:[
-      {img:"",title:""},
-      {img:"",title:""},
-      {img:"",title:""},
+      {img:"/assets/images/icon-figma.png",title:"Figma"},
+      {img:"/assets/images/icon-xd.png",title:"Adobe XD"},
     ]
   },{
     title:"Frontend Development",
     description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  ut labore et dolore magna aliqua.",
     img:"/assets/images/icon-frontend-fill.svg",
     tags:[
-      {img:"",title:""},
-      {img:"",title:""},
-      {img:"",title:""},
+      {img:"/assets/images/icon-react.png",title:"React Js"},
+      {img:"/assets/images/icon-tailwind.png",title:"Tailwind"},
     ]
   },{
     title:"Interactive Development",
     description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  ut labore et dolore magna aliqua.",
     img:"/assets/images/icon-inter-fill.svg",
     tags:[
-      {img:"",title:""},
-      {img:"",title:""},
-      {img:"",title:""},
+      {img:"/assets/images/icon-gsap.png",title:"GSAP"},
+      {img:"/assets/images/icon-three.png",title:"Three JS"},
     ]
   },{
     title:"CMS Development",
     description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  ut labore et dolore magna aliqua.",
     img:"/assets/images/icon-cms-fill.svg",
     tags:[
-      {img:"",title:""},
-      {img:"",title:""},
-      {img:"",title:""},
+      {img:"/assets/images/icon-strapi.png",title:"Strapi"},
+      {img:"/assets/images/icon-tailwind.png",title:"Wordpress"},
     ]
   },{
     title:"3d Design",
     description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  ut labore et dolore magna aliqua.",
     img:"/assets/images/icon-3d-fill.svg",
     tags:[
-      {img:"",title:""},
-      {img:"",title:""},
-      {img:"",title:""},
+      {img:"/assets/images/icon-spline.png",title:"Spline"},
+      {img:"/assets/images/icon-three.png",title:"Three JS"},
     ]
   },
 ]
